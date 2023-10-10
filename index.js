c0478d17fce87a4a2b39fa710d354debf9ce8c30
Karuga Mbugua <karugambugua@icloud.com>
2023-10-10 11:52:58 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(name, found) {
+    return name.filter(
+      (matched) => matched.toLowerCase() === found.toLowerCase()
+    );
+  }
+  
+  function fuzzyMatch(name, testingString) {
+    return name.filter(
+      (matched) =>
+        matched.toLowerCase().indexOf(testingString.toLowerCase()) === 0
+    );
+  }
+  
+  function matchName(name, foundName) {
+    return name.filter((record) => record.name === foundName);
+  }
